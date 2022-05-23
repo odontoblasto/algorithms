f5fa5081c8575b00015559da1954fe98c4cf898c
The MH Team <cedfv@cin.ufpe.br>
2022-05-23 20:21:18 -0300

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -7,6 +7,17 @@ class LinkedList:
     def __init__(self,head = None):
       self.head = head
     
+    #create a LIFO(last in,first out)- Stack
     def addNode(self,value):
-      self.head = Node (next,value)
+      self.head = Node (self.head,value)
       return self.head
+    #create a FIFO(first in,first out)- List or Array
+    def addNode(self,value):
+        if self.head:
+            pointer = self.head
+            while pointer.next:
+                pointer = pointer.next
+            pointer.next = Node(next,value)
+            return pointer.next # or Node(next,value)
+        self.head = Node(self.head,value)
+        return self.head # or Node(next,value)
