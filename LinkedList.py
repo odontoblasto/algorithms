450654806408f97ce44239e3d4bed07c7496a05c
The MH Team <cedfv@cin.ufpe.br>
2022-05-23 19:58:16 -0300

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,12 @@
-
+class Node:
+  def __init__(self, next = None, value = None):
+    self.next = next
+    self.value = value
+  
+  class LinkedList:
+  def __init__(self,head = None):
+    self.head = head
+    
+   def addNode(self,value):
+    self.head = Node (next,value)
+    return self.head
